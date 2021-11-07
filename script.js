286ee689f81c66afcb5e124d4a49cd609485fb52
Carolina Gomez <carolinagomezTX@gmail.com>
2021-11-07 10:04:58 -0600

    function

---
+++ script.js
@@ -0,0 +1,15 @@
+const text = "I am auto writing this and it's awesome!";
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0,index);
+
+    index++;
+
+    if (index > text.length - 1) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
